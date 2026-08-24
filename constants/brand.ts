b38b08787a946dfd9734b7827296318937547852
DeepLuxMed User <jmyocupicior@gmail.com>
2026-08-24 17:36:36 -0600

    chore: actualizar favicon e icono de marca a icono-ultrasonido.svg

--- constants/brand.ts
+++ constants/brand.ts
@@ -1,5 +1,5 @@
 export const APP_NAME = 'SONOTOX';
 export const APP_NAME_FULL = 'SONOTOX — Clínica de Toxina Botulínica';
 export const APP_TAGLINE = 'Asistente clínico';
-export const APP_LOGO_SRC = '/icons/icono-app-sonotox.png';
-export const APP_ICON_SRC = '/icons/icon-192.png';
+export const APP_LOGO_SRC = '/icons/icono-ultrasonido.svg';
+export const APP_ICON_SRC = '/icons/icono-ultrasonido.svg';
